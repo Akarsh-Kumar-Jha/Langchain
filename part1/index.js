606f63307796c6d0cf1b8fa5e044085a079c02a6
Akarsh-Kumar-Jha <akarshdbg@gmail.com>
2026-07-02 01:49:47 +0530

    With Structured Output Using Zod Validation

--- part1/index.js
+++ part1/index.js
@@ -1,13 +1,14 @@
 import {ChatGroq} from "@langchain/groq";
 import {ChatPromptTemplate, MessagesPlaceholder, PromptTemplate } from "@langchain/core/prompts";
 import dotenv from "dotenv";
+import { HumanMessage, SystemMessage,AIMessage } from "@langchain/core/messages";
+import * as z from "zod";
 
 dotenv.config();
 
 
 const model = new ChatGroq({
-    model:"openai/gpt-oss-120b",
-    temprature:2,
+    model:"openai/gpt-oss-120b"
 });
 
 
@@ -21,27 +22,42 @@ const model = new ChatGroq({
 
 // Chat prompt Template Example
 
-const history = [];
+// const history = [
+//     new SystemMessage("You are Narendar Modi So Answer Like ModiJi In Hinglish In Funny Manner."),  
+//     new HumanMessage("What is UCC bill?"),
+// ];
 
-const chatPrompt = ChatPromptTemplate.fromMessages([
-    ["system","You are Narendar Modi So Answer Like ModiJi In Hinglish In Funny Manner."],
-    new MessagesPlaceholder("history"),
-    ["human","Explain {query}"]
-]);
+// const chatPrompt = ChatPromptTemplate.fromMessages([
+//     ["system","You are Narendar Modi So Answer Like ModiJi In Hinglish In Funny Manner."],
+//     new MessagesPlaceholder("history"),
+//     ["human","Explain {query}"]
+// ]);
 
-const prompt = await chatPrompt.invoke({
-    query:"Who Is Rahul Gandhi?",
-    history
+// const prompt = await chatPrompt.invoke({
+//     query:"What is full form of that bill?",
+//     history
+// });
+
+
+const resultSchema = z.object({
+    name:z.string(),
+    occupation:z.string(),
+    awards:z.array(z.string()),
+    country:z.string(),
 });
 
-console.log("Prompt:",prompt.messages);
+const structuredModel = model.withStructuredOutput(resultSchema);
+
+
+const prompt = "Who Is Roman Reigns?";
+
+console.log("Prompt:",prompt);
 
 const callGroq = async() => {
     try {
-        const result = await model.invoke(prompt.messages);
-        history.push(result.content);
-        console.log("Groq:",result.content);
-        console.log("History:",history);
+        console.log('Model Called...')
+        const result = await structuredModel.invoke(prompt);
+        console.log("Groq:",result);
     } catch (error) {
         console.error("Error Occuered:",error);
     }
